afabe353ada5928bceead94562cfe2d0b1d85b7b
Khola Asghar <kholaasghar20@gmail.com>
2026-01-07 06:06:09 +0500

    customer

--- src/main/java/com/example/carrental/customer.java
+++ src/main/java/com/example/carrental/customer.java
@@ -1,4 +1,29 @@
 package com.example.carrental;
 
-public class customer {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class customer extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("Customer.fxml"));
+            Scene scene = new Scene(loader.load());
+
+            primaryStage.setTitle("Customer Details");
+            primaryStage.setScene(scene);
+            primaryStage.setResizable(false);
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
